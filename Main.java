972b5953d4365b8a0822c24556cd74506efc1d86
elliotsigal <145133154+elliotsigal@users.noreply.github.com>
2023-09-20 20:37:38 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -8,16 +8,22 @@ public class Main {
             System.out.println("");
             System.out.println("Remember pie is " + pie + "!");
             System.out.println("");
-            System.out.print("Please pick an option: 1) Division, 2) Finding the circumfernce 3) Adding by 1, 4) Subtracting by 1, 5) multiplying by pie, 6) quit: ");
+            System.out.print("Please pick an option: 1) Division, 2) Finding the circumfernce 3) Adding by 1, 4) Subtracting by 1, 5) multiplying by pie, 6)Break the system 7) quit: ");
             Scanner scan = new Scanner(System.in);
             int option = scan.nextInt();
             if (option == 1) {
                 System.out.print("Please write a numerator: ");
                 int numerator = scan.nextInt();
+                if (scan.hasNextInt()) {
+                    System.out.print("Always write your numbers in doubles!");
+                }
                 System.out.print("Please write a denominator: ");
+                if (scan.hasNextInt()) {
+                    System.out.print("Always write your numbers in doubles!");
+                }
                 int denominator = scan.nextInt();
                 try {
-                    int trial1 = (numerator / denominator);
+                    double trial1 = (double) numerator / denominator;
                     System.out.println(numerator / denominator);
                 } catch (ArithmeticException e) {
                     System.out.println("Cannot divide by zero!");
@@ -55,12 +61,38 @@ public class Main {
 
             }
             if (option == 6) {
+                int value = Integer.MAX_VALUE-1;
+                int i = 0;
+                while (true) {
+                    if (i == 50) {
+                        break;
+                    }
+                    System.out.println(value);
+                    i++;
+                    value++;
+
+                }
+                System.out.println("");
+                System.out.println("Oh no! You have broken the calculator!");
+                try {
+                    Thread.sleep(5000); // 2000ms = 2s
+                } catch (InterruptedException ex) {
+                    // Handle the exception
+                }
+                System.out.println("");
+                System.out.println("");
+                System.out.println("Nevermind, continue with the program.");
+
+                }
+            if (option == 7) {
                 quit = true;
 
+            }
+
+                }
             }
         }
 
 
-    }
-}
+
 
