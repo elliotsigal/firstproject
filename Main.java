16668d2c06a1b3cebb63a5276b6178abd721536b
elliotsigal <145133154+elliotsigal@users.noreply.github.com>
2023-09-22 10:19:49 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,49 +1,64 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int correct = 0;
         int wrong = 0;
-
-       System.out.println("Welcome to the brand new CST");
+        final double pi = 3.14;
+        System.out.println("Welcome to the brand new CST");
         try {
-            Thread.sleep(5000); // 2000ms = 2s
+            Thread.sleep(1000); // 2000ms = 2s
         } catch (InterruptedException ex) {
 
         }
-       System.out.println("Let's start");
+        System.out.println("Let's start");
         System.out.println("Pick a difficulty: 1) Hard 2) Medium 3) Easy: ");
         Scanner scan = new Scanner(System.in);
         int option = scan.nextInt();
-        boolean on = true;
         if (option == 1) {
             System.out.println("Wow, let's see how smart you really are!");
-            while ( on) {
-                while( on ){ // first number input
-                    System.out.println("Enter the first number. ");
-                    int answer1 = scan.nextInt();
-                    if ( answer1 < -2147483647 || answer1 > 2147483647 ) {
-                        System.out.println("First number is out of range, try again. (Must be a valid 32-bit integer");
-                        System.out.println("This one is tricky i'll give you another try");
+            System.out.println("What is the power of 2 that will get you " + (Integer.MAX_VALUE+1) + " when added by 1");
+            int answer1 = scan.nextInt();
+            if (answer1 !=31) {
+                wrong++;
 
-                    } else {
-                        if (answer1 !=2147483647) {
-                            wrong++;
-                            on = false;
                         } else {
                             System.out.println("Correct");
                             correct++;
-                            on = false;
-
 
                         }
-                    }
+                        System.out.println("Next question: What number does the denominator have to be for a Arthimetic exception to occur?");
+                        int answer2 = scan.nextInt();
+                        System.out.println("pick a numerator to see if you are correct");
+                        int numerator = scan.nextInt();
+                        try {
+                                int trial1 = numerator / answer2;
+                                System.out.println((double)trial1);
+                                System.out.println("Wrong!");
+                                wrong++;
+                        } catch (ArithmeticException e) {
+                            System.out.println("Correct!");
+                            correct++;
+
+                        }
+                        System.out.println("last question(!):What is the area of a circle with a diameter of 10?");
+                        double answer3 = scan.nextDouble();
+                        if (answer3 == Math.pow((10/2),2)*pi) {
+                            System.out.println("Correct!");
+                            correct++;
+                        } else {
+                            System.out.println("Wrong!");
+                            wrong++;
+                        }
+
+
+
+
 
                     }
+
                 }
-
             }
 
-    }
-}
+
+
+
